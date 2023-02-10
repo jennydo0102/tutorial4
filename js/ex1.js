fc425d2978af31b1983c73e87928236d92b94640
thanh.do <jennytd0201@gmail.com>
2023-02-09 18:44:14 -0800

    hw3

---
+++ js/ex1.js
@@ -0,0 +1,5 @@
+/* Tutorial 4
+   Example 1 JavaScript code
+*/
+
+console.log("Do's Output from Tutorial 4 Example 2");
---
+++ js/ex2.js
@@ -0,0 +1,6 @@
+/* Tutorial 4
+   Example 2 JavaScript code
+*/
+
+console.log("Do's Output from Tutorial 4 Example 2");
+
